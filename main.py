0b241f658a816411ccecd359972dabbdd9abcc32
Aryabhatt <bhattsahab314@gmail.com>
2023-09-05 22:02:27 +0530

    Added To Do comments in main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# To do lists
+# print logo
+# Print Compare A: {name}, {description}, from {country}.
+# print vs
+# print Against B: {name}, {description}, from {country}.
+# print Who has more Instagram followers? Type 'A' or 'B' :
+# If wrong print "Sorry, that's wrong. Final score: {score}"
+# If right, print "You're right! Current Score: 1
+# The correct answer from previous comparison becomes option A
+# And game continues
